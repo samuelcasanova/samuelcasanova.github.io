d64a01c5c7d4656593ea353ebcd34b1e7c8d4d12
Samuel Casanova <samuel.casanova@gmail.com>
2021-07-28 04:31:06 -0400

    Fixed some additional issue with dependencies and devdependencies and finally tested successfully

--- src/Components/UserInformation/UserService.js
+++ src/Components/UserInformation/UserService.js
@@ -1,17 +1,15 @@
-class UserService
-{
-    async getUser()
-    {
-        try {
-            return fetch('https://randomuser.me/api/').then( response => response.json()).then( response => 
-                new Promise( resolve => { 
-                    const userData = response.results[0];
-                    return resolve(userData);
-                }));
-        } catch (message) {
-            console.log(message);
-        }
+class UserService {
+  async getUser () {
+    try {
+      return fetch('https://randomuser.me/api/').then(response => response.json()).then(response =>
+        new Promise(resolve => {
+          const userData = response.results[0]
+          return resolve(userData)
+        }))
+    } catch (message) {
+      console.log(message)
     }
+  }
 }
 
-export default UserService;
+export default UserService
