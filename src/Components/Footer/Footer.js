a118d50ea185072d7f6de57218b1417793ab6dc1
Samuel Casanova <samuel.casanova@gmail.com>
2021-11-12 16:34:02 +0100

    Added menu

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,22 +3,13 @@ import './Footer.css'
 
 function Footer () {
   return (
-    <footer>
-      <ul>
-        <li>
-          <a href="/#/">Calendario</a>
-        </li>
-        <li>
-          <a href="/#/Categorias">Categorías</a>
-        </li>
-        <li>
-          <a href="https://www.fcf.cat/classificacio/2022/futbol-7/benjami-7-tercera-divisio/grup-6">Clasificación Alex</a>
-        </li>
-        <li>
-          <a href="https://www.fcf.cat/classificacio/2022/futbol-7/prebenjami-7/grup-18">Clasificación Victor</a>
-        </li>
-      </ul>
-    </footer>
+      <div className="Footer">
+        <footer>
+          <p>
+            Samuel Casanova, { new Date().getFullYear() }
+          </p>
+        </footer>
+      </div>
   )
 }
 
