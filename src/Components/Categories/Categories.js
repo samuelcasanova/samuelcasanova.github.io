3bd350300fd8d062d501c495b6836f21fee946c9
Samuel Casanova <samuel.casanova@gmail.com>
2021-11-13 12:25:29 +0100

    Some fixes for the menu component

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -12,48 +12,52 @@ class Categories extends React.Component {
     return (
       <div>
         <table>
-          <tr>
-            <th>Categoría</th>
-            <th>Edad</th>
-            <th>Modalidad</th>
-            <th>Quién</th>
-          </tr>
-          <tr>
-            <td>Pre-benjamines</td>
-            <td>6 y 7 años</td>
-            <td>Fútbol 7</td>
-            <td><FootballerCard footballer={victorFootballer}/></td>
-          </tr>
-          <tr>
-            <td>Benjamines</td>
-            <td>8 y 9 años</td>
-            <td>Fútbol 7</td>
-            <td><FootballerCard footballer={alexFootballer}/></td>
-          </tr>
-          <tr>
-            <td>Alevines</td>
-            <td>10 y 11 años</td>
-            <td>Fútbol 7</td>
-            <td><FootballerCard footballer={loupesFootballer}/></td>
-          </tr>
-          <tr>
-            <td>Infantiles</td>
-            <td>12 y 13 años</td>
-            <td>Fútbol 11</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Cadetes</td>
-            <td>14 y 15 años</td>
-            <td>Fútbol 11</td>
-            <td><FootballerCard footballer={loupesFootballer}/></td>
-          </tr>
-          <tr>
-            <td>Juveniles</td>
-            <td>16 a 19 años</td>
-            <td>Fútbol 11</td>
-            <td></td>
-          </tr>
+          <thead>
+            <tr>
+              <th>Categoría</th>
+              <th>Edad</th>
+              <th>Modalidad</th>
+              <th>Quién</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Pre-benjamines</td>
+              <td>6 y 7 años</td>
+              <td>Fútbol 7</td>
+              <td><FootballerCard footballer={victorFootballer}/></td>
+            </tr>
+            <tr>
+              <td>Benjamines</td>
+              <td>8 y 9 años</td>
+              <td>Fútbol 7</td>
+              <td><FootballerCard footballer={alexFootballer}/></td>
+            </tr>
+            <tr>
+              <td>Alevines</td>
+              <td>10 y 11 años</td>
+              <td>Fútbol 7</td>
+              <td><FootballerCard footballer={loupesFootballer}/></td>
+            </tr>
+            <tr>
+              <td>Infantiles</td>
+              <td>12 y 13 años</td>
+              <td>Fútbol 11</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Cadetes</td>
+              <td>14 y 15 años</td>
+              <td>Fútbol 11</td>
+              <td><FootballerCard footballer={loupesFootballer}/></td>
+            </tr>
+            <tr>
+              <td>Juveniles</td>
+              <td>16 a 19 años</td>
+              <td>Fútbol 11</td>
+              <td></td>
+            </tr>
+          </tbody>
         </table>
       </div>
     )
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -4,12 +4,17 @@ import './Menu.css'
 const Menu = () => {
   const [open, setOpen] = useState(false)
 
+  const setMenuState = () => {
+    setOpen(!open)
+    setMenuHeightAndTop()
+  }
+
   return (
     <div>
-      <button className={ open ? 'burger burger-open' : 'burger burger-close'} onClick={() => { setOpen(!open) }}>
+      <button className={ open ? 'burger burger-open' : 'burger burger-close'} onClick={ setMenuState }>
         <span className='materialicons'>{ open ? 'close' : 'menu' }</span>
       </button>
-      <div className={ open ? 'menu menu-open' : 'menu menu-close'}>
+      <div className={ open ? 'menu menu-open' : 'menu menu-close'} onClick={() => { setOpen(false) }}>
         <a href="/#/">Calendario</a>
         <a href="/#/Categorias">Categorías</a>
         <a href="https://www.fcf.cat/classificacio/2022/futbol-7/benjami-7-tercera-divisio/grup-6">Clasificación Alex</a>
@@ -18,4 +23,15 @@ const Menu = () => {
     </div>
   )
 }
+
+function setMenuHeightAndTop () {
+  const headerHeight = document.getElementsByTagName('header')[0].clientHeight
+  const viewportHeight = window.innerHeight
+  const menuHeight = viewportHeight - headerHeight
+
+  const menuElement = document.getElementsByClassName('menu')[0]
+  menuElement.style.height = menuHeight + 'px'
+  menuElement.style.top = headerHeight + 'px'
+}
+
 export default Menu
