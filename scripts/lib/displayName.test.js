32994938851a9a4915379cc9e8304bbb03d856ba
samuel.casanova <samuel.casanova@immfly.com>
2026-09-25 17:14:02 +0200

    Fix five team display names

--- scripts/lib/displayName.test.js
+++ scripts/lib/displayName.test.js
@@ -7,7 +7,12 @@ describe('getDisplayName', () => {
     ["FUNDACIÓ ACADEMIA F. L'HOSPITALET  B", "L'Hospitalet B"],
     ['ESCOLA DE FUTBOL STOITCHKOV-RACING SARRIA  B', 'Racing Sarria B'],
     ['Pª BARC. CINC COPES A', 'Cinc Copes A'],
-    ['HORTA, U.AT. C', 'Horta C']
+    ['HORTA, U.AT. C', 'Horta C'],
+    ['ESCOLA FUTBOL ALMOGAVERS A', 'Almogavers A'],
+    ['TARR, ASSOC.ESP.  A', 'Tarr A'],
+    ['VILA OLIMPICA CLUB ESP. A', 'Vila Olímpica A'],
+    ['FUNDACIO PRIVADA ASSOC. ESP. PRAT  A', 'Prat A'],
+    ['ESCUELA F.SAN PEDRO SAN PABLO A', 'San Pedro San Pablo A']
   ])('%s → %s', (rawName, displayName) => {
     expect(getDisplayName(rawName)).toBe(displayName)
   })
